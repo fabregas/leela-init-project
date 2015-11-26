2963e386c367f2ba0bfd366221a80c881572c50b
Konstantin Andrusenko <konstantin.andrusenko@zabs.co>
2015-11-26 14:36:42 +0200

    fixed js, css compilation and fonts moving

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,10 @@ var path = require('path');
 var rimraf = require('gulp-rimraf');
 var sourcemaps = require('gulp-sourcemaps');
 var less = require('gulp-less');
+var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var merge = require('merge-stream');
+var flatten = require('gulp-flatten');
 
 // Include plugins
 var plugins = require("gulp-load-plugins")({
@@ -15,44 +19,52 @@ var plugins = require("gulp-load-plugins")({
               });
 
 // Define default destination folder
-var dest = 'www/';
+var dest = 'www/compiled/';
 
 gulp.task('clean', function (cb) {
-  return gulp.src([
-        path.join(dest, 'js/modules.min.js'),
-        path.join(dest, 'css/modules.css'),
-      ], { read: false })
-     .pipe(rimraf());
+  return gulp.src(dest).pipe(rimraf());
 });
 
 gulp.task('js', function() {
-    gulp.src(plugins.mainBowerFiles())
-                .pipe(plugins.filter('*.js'))
-                .pipe(plugins.concat('modules.min.js'))
-                .pipe(plugins.uglify())
-                .pipe(gulp.dest(dest + 'js'));
+    bower_stream = gulp.src(plugins.mainBowerFiles())
+                   .pipe(plugins.filter('*.js'));
+
+    custom_stream = gulp.src('www/js/**/*.js')
+		    .pipe(flatten());
+
+    return merge(bower_stream, custom_stream)
+                   .pipe(plugins.concat('app.min.js'))
+                   .pipe(plugins.uglify())
+                   .pipe(gulp.dest(dest + 'js'));
 });
 
 gulp.task('css', function() {
-    gulp.src(plugins.mainBowerFiles())
-                .pipe(plugins.filter('*.css'))
-                .pipe(plugins.concat('modules.css'))
-                .pipe(plugins.uglify())
-                .pipe(gulp.dest(dest + 'css'));
+    css_stream = gulp.src(plugins.mainBowerFiles())
+                          .pipe(plugins.filter('*.css'));
+
+    sass_stream = gulp.src(plugins.mainBowerFiles())
+                           .pipe(plugins.filter('*.scss'))
+			   .pipe(sass());
+
+    less_stream = gulp.src(plugins.mainBowerFiles())
+			   .pipe(plugins.filter('*.less'))
+			   .pipe(less());
+
+    custom_stream = gulp.src('www/css/*.css');
+
+    return merge(sass_stream, css_stream, less_stream, custom_stream)
+		.pipe(cssmin())
+	        .pipe(plugins.concat('app.css.min'))
+	        .pipe(gulp.dest(dest + 'css'));
 });
 
-
-gulp.task("less", function(){
-    return gulp.src(plugins.mainBowerFiles())
-            .pipe(sourcemaps.init())
-            .pipe(plugins.filter('*.less'))
-            .pipe(less())
-            .pipe(plugins.concat("modules.less.css"))
-            .pipe(sourcemaps.write())
-            .pipe(gulp.dest(dest + 'css'));
+gulp.task('fonts', function () {
+    return gulp.src('./bower_components/**/*.{eot,svg,ttf,woff,woff2}')
+	    .pipe(flatten())
+            .pipe(gulp.dest(dest + 'fonts'));
 });
 
 gulp.task('default', function(done) {
-  var tasks = ['js', 'less', 'css'];
+  var tasks = ['js', 'css', 'fonts'];
   seq('clean', tasks, done);
 });
